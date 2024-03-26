a6472fd4ca2dabbf2c138a1944d156e1c38ef35a
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2024-03-26 15:35:55 +0800

    test controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ConnectionTestController;
 
@@ -9,7 +10,7 @@ use App\Http\Controllers\ConnectionTestController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServicseProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
 */
@@ -17,6 +18,8 @@ use App\Http\Controllers\ConnectionTestController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', [TestController::class, 'test']);
 
 Route::get('/test-connection', [ConnectionTestController::class, 'testConnection']);
 
