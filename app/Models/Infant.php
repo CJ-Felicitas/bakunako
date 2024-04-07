f3613cd5bc669212d9bb779b94bc05434915d9fd
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2024-04-07 16:10:13 +0800

    new models

---
+++ app/Models/Infant.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Infant extends Model
+{
+    use HasFactory;
+    protected $table = 'infants';
+    protected $fillable = [
+        'infant_firstname',
+        'infant_lastname',
+        'infant_middlename',
+        'date_of_birth',
+        'place_of_birth',
+        'father_firstname',
+        'father_lastname',
+        'father_middlename',
+        'mother_firstname',
+        'mother_lastname',
+        'mother_middlename',
+        'address',
+        'sex',
+        'user_id',
+        'created_at',
+        'updated_at'
+    ];
+}
