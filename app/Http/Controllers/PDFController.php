7487a40d7256aca5fd4e17040afd825f318fb381
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2024-04-23 00:26:16 +0800

    emergency save

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -5,18 +5,25 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Barryvdh\DomPDF\Facade\Pdf;
-
+use App\Models\Infant;
+use App\Models\Schedule;
 class PDFController extends Controller
 {
     public function generatePDF(){
         $users = User::get();
+        $infants = Infant::get();
+        $schedule = Schedule::get();
+
         $data = [
-            'title' => 'Welcome to LaravelTuts.com',
+            'title' => "Infant's Information",
             'date' => date('m/d/Y'),
-            'users' => $users
+            'users' => $users,
+            'infants' => $infants,
+            'schedule' => $schedule
         ]; 
 
         $pdf = PDF::loadView('myPDF', $data);
-        return $pdf->download('laraveltuts.pdf');
+        
+        return $pdf->download('bakunako.pdf');
     }
 }
